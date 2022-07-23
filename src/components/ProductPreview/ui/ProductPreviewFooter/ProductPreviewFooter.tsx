4678d26a41416a8c39a5681f0300923a6e918dbd
Josh D <jjdate@live.com>
2022-07-23 13:19:05 -0400

    removed unused import

--- src/components/ProductPreview/ui/ProductPreviewFooter/ProductPreviewFooter.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const ProductPreviewFooter = () => {
-  return <div>ProductPreviewFooter</div>;
-};
-export default ProductPreviewFooter;
--- src/components/ProductPreview/ui/ProductPreviewImage/ProductPreviewImage.tsx
+++ src/components/ProductPreview/ui/ProductPreviewImage/ProductPreviewImage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState } from "react";
 // @ts-ignore
 import productImage from "../../../../assets/images/image-product-mobile.jpg";
 // @ts-ignore
